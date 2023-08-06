94ebe1db5a365f82cbf9b075bcf3c21dfb0722fc
Dang Hieu <danghieutieumai@gmail.com>
2023-08-07 02:26:20 +0700

    add style project

---
+++ main.js
@@ -0,0 +1,10 @@
+var section = document.querySelectorAll('section');
+var header = document.querySelectorAll('.header__link');
+
+section.forEach(function(item) {
+    var top = window.scrollY;
+    var offset = item.offsetTop;
+    var height = item.offsetHeight;
+
+    if
+})
