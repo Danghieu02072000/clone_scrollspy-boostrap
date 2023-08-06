293ad9e6abf205a8228b0454f587b22c0fc040c4
Dang Hieu <danghieutieumai@gmail.com>
2023-08-07 02:54:38 +0700

    handle scroll

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 var section = document.querySelectorAll('section');
 var header = document.querySelectorAll('.header__link');
 
-section.forEach(function(item) {
-    var top = window.scrollY;
-    var offset = item.offsetTop;
-    var height = item.offsetHeight;
-
-    if
-})
+window.onscroll = function() {
+    section.forEach(function(item) {
+        var top = window.scrollY;
+        var offset = item.offsetTop - 50;
+        var height = item.offsetHeight;
+        let id = item.getAttribute('id');
+        if(top > offset && top < offset+height) {
+            header.forEach(function(header) {
+                header.classList.remove('active');
+                document.querySelector('.header__link[href*='+id+']').classList.add('active')
+            })
+        }
+    })
+}
